d85332ff4face8d6b42a54a5cc0e7135eea91446
Lauro Lyra <laurolyra@gmail.com>
2025-08-06 12:06:39 -0300

    primeira conversa com o groq

---
+++ groq_chat.py
@@ -0,0 +1,20 @@
+import os
+from groq import Groq
+# import pprint
+
+client = Groq(
+    api_key=os.environ.get('GROQ_API_KEY')
+)
+
+chat = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explique a importância de modelos de linguagens rapidos"
+        }
+    ],
+    model="llama-3.3-70b-versatile"
+)
+
+
+print(chat.choices[0].message.content)
