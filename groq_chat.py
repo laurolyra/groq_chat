f9bf2583a2ada6599eefdddafca307c3bd7b99b0
Lauro Lyra <laurolyra@gmail.com>
2025-08-08 10:47:14 -0300

    pergunta

--- groq_chat.py
+++ groq_chat.py
@@ -1,20 +1,27 @@
 import os
 from groq import Groq
-# import pprint
 
-client = Groq(
-    api_key=os.environ.get('GROQ_API_KEY')
-)
+def interacao_chat(pergunta):
+    client = Groq(
+        api_key=os.environ.get('GROQ_API_KEY')
+    )
 
-chat = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explique a importância de modelos de linguagens rapidos"
-        }
-    ],
-    model="llama-3.3-70b-versatile"
-)
+    chat = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": pergunta
+            }
+        ],
+        model="llama-3.3-70b-versatile"
+    )
 
+    return chat.choices[0].message.content
 
-print(chat.choices[0].message.content)
+if __name__ == "__main__":
+    while True:
+        pergunta = input("digite a sua pergunta (digite 'sair' para encerrar)\n")
+        if pergunta == 'sair':
+            print('encerrando...\n\n')
+            break
+        print(interacao_chat(pergunta))
